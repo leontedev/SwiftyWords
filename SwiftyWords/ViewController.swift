5247e53ba80abcd7ff37fe06394cd0821d0cd4d0
Mihail87 <leontemihai@gmail.com>
2019-09-13 16:09:03 +0300

    Project setup

---
+++ SwiftyWords/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftyWords
+//
+//  Created by Mihai Leonte on 9/13/19.
+//  Copyright © 2019 Mihai Leonte. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
